9cfd9c47b7c111243b17b810695550036a26ef59
anikahammedkhan <anikkhan1105@gmail.com>
2025-10-15 18:34:19 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <div className="p-4">Your code goes here... show us your magic ✨</div>
+      <video className="w-[300px] ml-4" src="/demo.mp4" controls />;
+    </div>
+  );
+};
+
+export default App;
